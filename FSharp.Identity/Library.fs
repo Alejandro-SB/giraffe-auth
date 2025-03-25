857b23345f9c74e675297098692dc9e62d72cda9
Alejandro Sanchez Bastida <alejandro.sanchez.bastida@gmail.com>
2025-03-25 10:15:19 +0100

    Initial

---
+++ FSharp.Identity/Library.fs
@@ -0,0 +1,61 @@
+﻿namespace FSharp.Identity
+
+open Microsoft.AspNetCore.Identity
+
+module FsharpIdentity =
+    type IdentityUser = { Id: string; Name: string }
+
+    type UserManager =
+        interface IUserStore<IdentityUser> with
+            member this.CreateAsync
+                (user: IdentityUser, cancellationToken: System.Threading.CancellationToken)
+                : System.Threading.Tasks.Task<IdentityResult> =
+                raise (System.NotImplementedException())
+
+            member this.DeleteAsync
+                (user: IdentityUser, cancellationToken: System.Threading.CancellationToken)
+                : System.Threading.Tasks.Task<IdentityResult> =
+                raise (System.NotImplementedException())
+
+            member this.Dispose() : unit =
+                raise (System.NotImplementedException())
+
+            member this.FindByIdAsync
+                (userId: string, cancellationToken: System.Threading.CancellationToken)
+                : System.Threading.Tasks.Task<IdentityUser> =
+                raise (System.NotImplementedException())
+
+            member this.FindByNameAsync
+                (normalizedUserName: string, cancellationToken: System.Threading.CancellationToken)
+                : System.Threading.Tasks.Task<IdentityUser> =
+                raise (System.NotImplementedException())
+
+            member this.GetNormalizedUserNameAsync
+                (user: IdentityUser, cancellationToken: System.Threading.CancellationToken)
+                : System.Threading.Tasks.Task<string> =
+                raise (System.NotImplementedException())
+
+            member this.GetUserIdAsync
+                (user: IdentityUser, cancellationToken: System.Threading.CancellationToken)
+                : System.Threading.Tasks.Task<string> =
+                raise (System.NotImplementedException())
+
+            member this.GetUserNameAsync
+                (user: IdentityUser, cancellationToken: System.Threading.CancellationToken)
+                : System.Threading.Tasks.Task<string> =
+                raise (System.NotImplementedException())
+
+            member this.SetNormalizedUserNameAsync
+                (user: IdentityUser, normalizedName: string, cancellationToken: System.Threading.CancellationToken)
+                : System.Threading.Tasks.Task =
+                raise (System.NotImplementedException())
+
+            member this.SetUserNameAsync
+                (user: IdentityUser, userName: string, cancellationToken: System.Threading.CancellationToken)
+                : System.Threading.Tasks.Task =
+                raise (System.NotImplementedException())
+
+            member this.UpdateAsync
+                (user: IdentityUser, cancellationToken: System.Threading.CancellationToken)
+                : System.Threading.Tasks.Task<IdentityResult> =
+                raise (System.NotImplementedException())
---
+++ Program.fs
@@ -0,0 +1,141 @@
+module giraffe_auth.App
+
+open System
+open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Cors.Infrastructure
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open Microsoft.EntityFrameworkCore
+open Microsoft.AspNetCore.Identity
+
+// ---------------------------------
+// Models
+// ---------------------------------
+
+type Message =
+    {
+        Text : string
+    }
+
+// ---------------------------------
+// Views
+// ---------------------------------
+
+module Views =
+    open Giraffe.ViewEngine
+
+    let layout (content: XmlNode list) =
+        html [] [
+            head [] [
+                title []  [ encodedText "giraffe_auth" ]
+                link [ _rel  "stylesheet"
+                       _type "text/css"
+                       _href "/main.css" ]
+            ]
+            body [] content
+        ]
+
+    let partial () =
+        h1 [] [ encodedText "giraffe_auth" ]
+
+    let index (model : Message) =
+        [
+            partial()
+            p [] [ encodedText model.Text ]
+        ] |> layout
+
+// ---------------------------------
+// Web app
+// ---------------------------------
+
+let indexHandler (name : string) =
+    let greetings = sprintf "Hello %s, from Giraffe!" name
+    let model     = { Text = greetings }
+    let view      = Views.index model
+    htmlView view
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route "/" >=> indexHandler "world"
+                routef "/hello/%s" indexHandler
+            ]
+        setStatusCode 404 >=> text "Not Found" ]
+
+// ---------------------------------
+// Error handler
+// ---------------------------------
+
+let errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+// ---------------------------------
+// Config and Main
+// ---------------------------------
+
+let configureCors (builder : CorsPolicyBuilder) =
+    builder
+        .WithOrigins(
+            "http://localhost:5000",
+            "https://localhost:5001")
+       .AllowAnyMethod()
+       .AllowAnyHeader()
+       |> ignore
+
+let configureApp (app : IApplicationBuilder) =
+    let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
+    (match env.IsDevelopment() with
+    | true  ->
+        app.UseDeveloperExceptionPage()
+    | false ->
+        app .UseGiraffeErrorHandler(errorHandler)
+            .UseHttpsRedirection())
+        .UseCors(configureCors)
+        .UseStaticFiles()
+        .UseGiraffe(webApp)
+
+let configureServices (services : IServiceCollection) =
+    services.AddCors()    |> ignore
+    services.AddGiraffe() |> ignore
+
+    //services.AddIdentity<IdentityUser, IdentityRole>(fun options -> 
+    //    options.Password.RequireLowercase <- true
+    //    options.Password.RequireUppercase <- true
+    //    options.Password.RequireDigit <- true
+    //    options.Lockout.MaxFailedAccessAttempts <- 5
+    //    options.Lockout.DefaultLockoutTimeSpan <- TimeSpan.FromMinutes(15L)
+    //    options.User.RequireUniqueEmail <- true
+    //    // enable this if we use email verification 
+    //    // options.SignIn.RequireConfirmedEmail <- true;
+    //    )
+    //// tell asp.net identity to use the above store
+    //    .AddDefaultTokenProviders() // need for email verification token generation
+    //    |> ignore
+
+let configureLogging (builder : ILoggingBuilder) =
+    builder.AddConsole()
+           .AddDebug() |> ignore
+
+[<EntryPoint>]
+let main args =
+    let contentRoot = Directory.GetCurrentDirectory()
+    let webRoot     = Path.Combine(contentRoot, "WebRoot")
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .UseContentRoot(contentRoot)
+                    .UseWebRoot(webRoot)
+                    .Configure(Action<IApplicationBuilder> configureApp)
+                    .ConfigureServices(configureServices)
+                    .ConfigureLogging(configureLogging)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
